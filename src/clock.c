0509991180a9564d2ac205d48093a4ecb4bd3404
Simon Petit <nomisp96@hotmail.fr>
2022-05-04 08:38:51 +0200

    replacing timediff by standard difftime, waiting for the rework of rtkcmn

--- src/clock.c
+++ src/clock.c
@@ -4,6 +4,7 @@
 int CLOCK_comparePreciseClocks(const void *p1, const void *p2)
 {
     PreciseClock_t *q1=(PreciseClock_t *)p1,*q2=(PreciseClock_t *)p2;
-    double tt=timediff(q1->time,q2->time);
+    // double tt=timediff(q1->time,q2->time);
+    double tt=difftime(q1->time.time,q2->time.time)+q1->time.sec-q2->time.sec;
     return tt<-1E-9?-1:(tt>1E-9?1:q1->index-q2->index);
 }
